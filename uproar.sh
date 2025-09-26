cf8e1e8f43110e83660df1a20016c64c573859ea
SylveonDev <sylveondev@yahoo.com>
2025-09-26 12:43:59 -0400

    Fix missing closes ig lol

--- uproar.sh
+++ uproar.sh
@@ -5,9 +5,9 @@
 echo "(1/3) First thing first, Uproar needs to know where to install the system."
 echo "      Warning: Uproar will erase the entire disk, so enter the correct disk."
 sleep 3
-echo "---
+echo "---"
 fdisk -l
-echo "---
+echo "---"
 echo "Enter the disk to write to (For example: /dev/sda)"
 read DISK
 if [ -x DISK ]; then
